253be1f75e0e4cce82658a685f9fb1daa0461fee
Zach <zach.chang@live.com>
2020-02-01 11:55:36 -0500

    Drag Drop Rotate Working

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static Junk selected;
+    public static PlayerController instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Debug.LogError("Two Player Controllers! Agh!");
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Mouse up!");
+            if (selected) selected.Deselect();
+            selected = null;
+        }
+
+        float rotate = Input.GetAxisRaw("Rotate");
+
+        if (rotate != 0)
+        {
+            //Debug.Log("Rotate is currently " + rotate);
+            if (selected)
+            {
+                selected.Rotate(rotate);
+            }
+
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (selected)
+        {
+            // drag it towards the mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selected.MoveTowards(mousePos);
+
+        }
+
+    }
+
+    public static void SetJunk(Junk junk)
+    {
+        selected = junk;
+    }
+
+
+}
---
+++ Assets/Scripts/Junk.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Junk : MonoBehaviour, IPointerDownHandler
+{
+    public static float rotationSpeed = 300f;
+    private Rigidbody2D rb2d;
+    private TargetJoint2D tj2d;
+
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        tj2d = GetComponent<TargetJoint2D>();
+    }
+
+    public void MoveTowards(Vector2 destination)
+    {
+        tj2d.enabled = true;
+        tj2d.target = destination;
+    }
+
+    public void Deselect()
+    {
+        tj2d.enabled = false;
+        rb2d.constraints = RigidbodyConstraints2D.None;
+        rb2d.velocity = new Vector2();
+    }
+
+    public void Select()
+    {
+        tj2d.enabled = true;
+        rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+        PlayerController.SetJunk(this);
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        Select();
+    }
+
+    public void Rotate(float rotate)
+    {
+        float deg = rb2d.rotation;
+        Debug.Log("Rotation is currently " + deg);
+        deg += (-1 * rotate * rotationSpeed * Time.deltaTime);
+        Debug.Log("Attempting to set rotation to " + deg);
+        rb2d.SetRotation(deg);
+        Debug.Log("Rotation is now " + rb2d.rotation);
+    }
+}
